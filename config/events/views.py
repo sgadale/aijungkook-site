ff6c29377e7a0463d6ad7c696a9cb928d6f1f86b
sgadale <119937503+sgadale@users.noreply.github.com>
2026-01-21 11:02:56 +0530

    Delete config directory

--- config/events/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Event
-
-
-def event_list(request):
-    events = Event.objects.all()
-    return render(request, 'events/event_list.html', {'events': events})
--- config/gallery/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Gallery
-
-
-def gallery_list(request):
-    images = Gallery.objects.all()
-    return render(request, 'gallery/gallery_list.html', {'images': images})
